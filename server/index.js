54cea73e8355fdc58ab63d3e5f882ff1d18d874e
LaToddra Belton <latoddra.belton@codeimmersives.com>
2021-04-29 06:15:45 -0500

    test push

---
+++ server/index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import postRoutes from './routes/posts.js'
+
+//initializes the app,uses all the functions on app
+const app = express();
+
+// every route in route inside of 'postRoutes' will start with 'posts' 
+app.use('/posts', postRoutes)
+          
+//not sure why it's saying it's depreciated 
+app.use(bodyParser.json({limit: "30mb", extended : true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+
+//mongodb connection
+const CONNECTION_URL = 'mongodb+srv://jsnewbie:yNsNMfcHVBme1cld@cluster0.t8bwv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL,{useNewUrlParser: true, useUnifiedTopology:true})
+    .then(()=>app.listen(PORT,()=>console.log(`Server running on port:${PORT}`)))
+    .catch((error)=>console.log(error.message))
+    
+//makes sure no warnings appear in the console
+mongoose.set('useFindAndModify', false)
